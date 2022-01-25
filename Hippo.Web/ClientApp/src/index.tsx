f8e9b5f602e349220b19436a7b70695265890d61
Scott Kirkland <srkirkland@gmail.com>
2022-01-25 15:47:57 -0800

    sass in the house

--- Hippo.Web/ClientApp/src/index.tsx
+++ Hippo.Web/ClientApp/src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import "bootstrap/dist/css/bootstrap.css";
+import "./sass/hippo.scss";
 
 const rootElement = document.getElementById("root");
 
