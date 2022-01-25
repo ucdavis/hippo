73535490c642fcb34651832cf1a4f31c0af0ebf9
Scott Kirkland <srkirkland@gmail.com>
2022-01-25 15:53:05 -0800

    functional component

--- Hippo.Web/ClientApp/src/App.tsx
+++ Hippo.Web/ClientApp/src/App.tsx
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Sample2 } from "./components/Sample2";
 
-export default class App extends Component {
-  static displayName = App.name;
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/page2" component={Sample2} />
+    </Switch>
+  );
+};
 
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/page2" component={Sample2} />
-      </Switch>
-    );
-  }
-}
+export default App;
