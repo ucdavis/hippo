8ddcc1e8648d27639d5290f857882be90d4e6332
Scott Kirkland <srkirkland@gmail.com>
2022-01-25 15:59:59 -0800

    sample navbar too

--- Hippo.Web/ClientApp/src/App.tsx
+++ Hippo.Web/ClientApp/src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
+import { AppNav } from "./AppNav";
 import { Home } from "./components/Home";
 import { Sample2 } from "./components/Sample2";
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/page2" component={Sample2} />
-    </Switch>
+    <>
+      <AppNav></AppNav>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/page2" component={Sample2} />
+      </Switch>
+    </>
   );
 };
 
---
+++ Hippo.Web/ClientApp/src/AppNav.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import {
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+} from "reactstrap";
+
+export const AppNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="light" expand="md" light>
+      <NavbarBrand href="/">HiPPO</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="me-auto" navbar>
+          <NavItem>
+            <NavLink href="/components/">Components</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reactstrap/reactstrap">
+              GitHub
+            </NavLink>
+          </NavItem>
+          <UncontrolledDropdown inNavbar nav>
+            <DropdownToggle caret nav>
+              Options
+            </DropdownToggle>
+            <DropdownMenu end>
+              <DropdownItem>Option 1</DropdownItem>
+              <DropdownItem>Option 2</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+        <NavbarText>Simple Text</NavbarText>
+      </Collapse>
+    </Navbar>
+  );
+};
