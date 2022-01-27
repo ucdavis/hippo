da1493aa9b02b23c3886dbe52b6baa91a51f10ac
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-01-27 10:29:08 -0800

    Sample tests

---
+++ Test/Helpers/CreateValidEntities.cs
@@ -0,0 +1,25 @@
+﻿using Hippo.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Helpers
+{
+    public class CreateValidEntities
+    {
+        public static User User(int counter)
+        {
+            var rtValue = new User();
+            rtValue.Id = counter;
+            rtValue.FirstName = $"FirstName{counter}";
+            rtValue.LastName = $"LastName{counter}";
+            rtValue.Email = $"Email{counter}@fake.com";
+            rtValue.Iam = $"Iam{counter}";
+            rtValue.Kerberos = $"Kerberos{counter}";
+
+            return rtValue;
+        }
+    }
+}
