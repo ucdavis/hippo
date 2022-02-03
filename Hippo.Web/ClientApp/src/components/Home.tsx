6b1d09957bb768396136ddb5a71d1201cc73edb3
Cal <cydoval@mac.com>
2022-02-03 13:11:19 -0700

    svg testing

--- Hippo.Web/ClientApp/src/components/Home.tsx
+++ Hippo.Web/ClientApp/src/components/Home.tsx
@@ -6,56 +6,25 @@ export class Home extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li>
-            <Link to="/page2">Go to sample page 2</Link>
-          </li>
-          <li>
-            <a href="https://get.asp.net/">ASP.NET Core</a> and{" "}
-            <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">
-              C#
-            </a>{" "}
-            for cross-platform server-side code
-          </li>
-          <li>
-            <a href="https://facebook.github.io/react/">React</a> for
-            client-side code
-          </li>
-          <li>
-            <a href="http://getbootstrap.com/">Bootstrap</a> for layout and
-            styling
-          </li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li>
-            <strong>Client-side navigation</strong>. For example, click{" "}
-            <em>Counter</em> then <em>Back</em> to return here.
-          </li>
-          <li>
-            <strong>Development server integration</strong>. In development
-            mode, the development server from <code>create-react-app</code> runs
-            in the background automatically, so your client-side resources are
-            dynamically built on demand and the page refreshes when you modify
-            any file.
-          </li>
-          <li>
-            <strong>Efficient production builds</strong>. In production mode,
-            development-time features are disabled, and your{" "}
-            <code>dotnet publish</code> configuration produces minified,
-            efficiently bundled JavaScript files.
-          </li>
-        </ul>
-        <p>
-          The <code>ClientApp</code> subdirectory is a standard React
-          application based on the <code>create-react-app</code> template. If
-          you open a command prompt in that directory, you can run{" "}
-          <code>npm</code> commands such as <code>npm test</code> or{" "}
-          <code>npm install</code>.
-        </p>
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <p>
+            Welcome, <span className="status-color">Calvin</span>
+            <br />
+            If you’d like access to HiPPO please answer the questions below
+          </p>
+
+          <hr />
+          <div className="form-group">
+            <label>Who is sponsoring your account?</label>
+            <select className="form-select" aria-label="Default select example">
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+        </div>
       </div>
     );
   }
