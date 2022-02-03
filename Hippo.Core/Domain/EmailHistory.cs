70923df3ef62ef5009b47b120750c8b424e2863f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-02-03 09:15:35 -0800

    Save email to db when send/failure

---
+++ Hippo.Core/Domain/EmailHistory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hippo.Core.Domain
+{
+    public class EmailHistory
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool IsSuccess { get; set; }
+        public DateTime CreatedOn { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string EmailTo { get; set; }
+        [MaxLength(250)]
+        public string EmailCC { get; set; }
+        [Required]
+        [MaxLength(200)]        
+        public string Subject { get; set; }
+        [Required]
+        public string Body { get; set; }
+        public string AltText { get; set; }
+        public string ErrorText { get; set; }
+        
+    }
+}
