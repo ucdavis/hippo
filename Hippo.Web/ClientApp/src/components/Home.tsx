f52fb759b65a67af68abde6cfd72a1a6f4912dce
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-02-07 13:41:39 -0800

    Change to function, use user's name

--- Hippo.Web/ClientApp/src/components/Home.tsx
+++ Hippo.Web/ClientApp/src/components/Home.tsx
@@ -1,42 +1,41 @@
-import React, { Component } from "react";
+import { Component, useContext } from "react";
 import { Link } from "react-router-dom";
+import AppContext from "../Shared/AppContext";
 
-export class Home extends Component {
-  static displayName = Home.name;
+export const Home = () => {
+  const user = useContext(AppContext).user;
 
-  render() {
-    return (
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <h3>
-            Welcome, <span className="status-color">Calvin</span>
-          </h3>
-          <p>If you’d like access to HiPPO please answer the questions below</p>
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-6">
+        <h3>
+          Welcome, <span className="status-color">{user.detail.firstName}</span>
+        </h3>
+        <p>If you’d like access to HiPPO please answer the questions below</p>
 
-          <hr />
-          <div className="form-group">
-            <label>Who is sponsoring your account?</label>
-            <select className="form-select" aria-label="Default select example">
-              <option selected>Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-            <p className="form-helper">Help text</p>
-          </div>
-          <div className="form-group">
-            <label className="form-label">What is your SSH key</label>
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-            ></textarea>
-            <p className="form-helper">Help text</p>
-          </div>
-          <a href="#" className="btn btn-primary">
-            Submit
-          </a>
+        <hr />
+        <div className="form-group">
+          <label>Who is sponsoring your account?</label>
+          <select className="form-select" aria-label="Default select example">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+          <p className="form-helper">Help text</p>
         </div>
+        <div className="form-group">
+          <label className="form-label">What is your SSH key</label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+          ></textarea>
+          <p className="form-helper">Help text</p>
+        </div>
+        <a href="#" className="btn btn-primary">
+          Submit
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
