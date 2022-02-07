b2b8c628f27ccf2e3b7c233c00aa9afe096c808e
Cal <cydoval@mac.com>
2022-02-07 12:36:05 -0700

    color animation test

--- Hippo.Web/ClientApp/src/components/Home.tsx
+++ Hippo.Web/ClientApp/src/components/Home.tsx
@@ -8,11 +8,10 @@ export class Home extends Component {
     return (
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <p>
+          <h3>
             Welcome, <span className="status-color">Calvin</span>
-            <br />
-            If you’d like access to HiPPO please answer the questions below
-          </p>
+          </h3>
+          <p>If you’d like access to HiPPO please answer the questions below</p>
 
           <hr />
           <div className="form-group">
@@ -23,7 +22,19 @@ export class Home extends Component {
               <option value="2">Two</option>
               <option value="3">Three</option>
             </select>
+            <p className="form-helper">Help text</p>
           </div>
+          <div className="form-group">
+            <label className="form-label">What is your SSH key</label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+            ></textarea>
+            <p className="form-helper">Help text</p>
+          </div>
+          <a href="#" className="btn btn-primary">
+            Submit
+          </a>
         </div>
       </div>
     );
