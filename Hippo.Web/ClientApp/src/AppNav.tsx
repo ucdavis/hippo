4e5b8538c675479e8d99ea7c933d7c64e3686a5f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-02-09 09:38:58 -0800

    Remove logout and area that shows full name

--- Hippo.Web/ClientApp/src/AppNav.tsx
+++ Hippo.Web/ClientApp/src/AppNav.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import {
   Collapse,
   DropdownItem,
@@ -13,12 +13,10 @@ import {
   NavLink,
   UncontrolledDropdown,
 } from "reactstrap";
-import AppContext from "./Shared/AppContext";
 import HippoLogo from "./Shared/hippoLogo";
 
 export const AppNav = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const user = useContext(AppContext).user;
   const toggle = () => setIsOpen(!isOpen);
 
   return (
@@ -31,17 +29,6 @@ export const AppNav = () => {
           HiPPO
         </h1>
         <p className="lede">High Performance Personnel Onboarding</p>
-        <p className="login">
-          {user.detail.name}
-          <span className="discreet">
-            {" "}
-            <form action="/Account/Logout" method="post" id="logoutForm">
-              <button className="btn btn-link btn-sm" type="submit">
-                • log out
-              </button>
-            </form>
-          </span>
-        </p>
       </div>
     </div>
   );
