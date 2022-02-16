8a4b8945fd568c04d2e3c0c80ea7a2beef5b8d6c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-02-16 08:36:38 -0800

    Post model

---
+++ Hippo.Web/Models/AccountRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Hippo.Web.Models
+{
+    public class AccountRequestModel
+    {
+        public int SponsorId { get; set; }
+        public string SponsorName { get; set; } = String.Empty; //As an extra check? Or just go with the SponsorId?
+        public string SshKey { get; set; } = String.Empty;
+    }
+}
