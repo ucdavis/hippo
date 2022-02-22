d1dc1ccc3f7aaea77e3713364536226a56c78131
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-02-22 10:57:47 -0800

    copy/paste comments....

--- Hippo.Core/Services/AccessConfig.cs
+++ Hippo.Core/Services/AccessConfig.cs
@@ -11,9 +11,7 @@ namespace Hippo.Core.Services
             {
                 // System can access anything
                 AccessCodes.SystemAccess => new[] { RoleCodes.SystemRole },
-                // FieldManager can access anything restricted to FieldManager, Supervisor, Worker or PI roles
                 AccessCodes.AdminAccess => new[] { RoleCodes.AdminRole },
-                // Supervisor can access anything restricted to Supervisor, Worker or PI roles
                 _ => throw new ArgumentException($"{nameof(accessCode)} is not a valid {nameof(AccessCodes)} constant")
             };
         }
