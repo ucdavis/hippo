21da04aa812addcc6b498e637479d2d6ae4ff7dc
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-02-23 14:02:33 -0800

    Show the use name
    Resolve #67

--- Hippo.Web/ClientApp/src/components/AccountInfo.tsx
+++ Hippo.Web/ClientApp/src/components/AccountInfo.tsx
@@ -4,11 +4,15 @@ import AppContext from "../Shared/AppContext";
 
 export const AccountInfo = () => {
   const [{ account }] = useContext(AppContext);
+  const [context, setContext] = useContext(AppContext);
 
   return (
     <div className="row justify-content-center">
       <div className="col-md-6">
-        <p>Welcome -- you already have an account, enjoy farm</p>
+        <p>
+          Welcome {context.user.detail.firstName} you already have an account,
+          enjoy farm
+        </p>
         {account.canSponsor && (
           <Link to="/approve">Click here to view any pending approvals</Link>
         )}
