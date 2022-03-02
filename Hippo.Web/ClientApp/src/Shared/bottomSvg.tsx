579a55b165acc96e7be73fb8e090d62e209acdb2
Cal <cydoval@mac.com>
2022-03-02 14:54:58 -0700

    animations

--- Hippo.Web/ClientApp/src/Shared/bottomSvg.tsx
+++ Hippo.Web/ClientApp/src/Shared/bottomSvg.tsx
@@ -7,158 +7,153 @@ export const BottomSvg = () => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <title>geo</title>
-    <g id="Main" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g id="Main" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
       <g id="Artboard" transform="translate(-131.000000, -89.000000)">
         <g
           id="geo"
           transform="translate(642.376263, 129.137729) rotate(-180.000000) translate(-642.376263, -129.137729) translate(132.861131, 90.129462)"
         >
           <g
-            id="geo-foot"
-            transform="translate(509.515132, 39.008267) rotate(-180.000000) translate(-509.515132, -39.008267) "
+            id="color-three"
+            transform="translate(492.333485, 39.008267) rotate(-180.000000) translate(-492.333485, -39.008267) translate(44.720753, 0.000000)"
+            fill="#2C0C3F"
             stroke="#FFFFFF"
-            strokeLinejoin="round"
-            strokeWidth="2"
+            stroke-linejoin="round"
+            stroke-width="2"
           >
-            <g
-              id="color-three"
-              transform="translate(79.084046, 0.000000)"
-              fill="#2C0C3F"
-            >
-              <polygon
-                id="cut3"
-                points="20.1458332 48.3949995 50.8965407 76.1330022 87.4561192 34.5943967"
-              ></polygon>
-              <polygon
-                id="cut3"
-                points="143.01306 78.0165342 87.4561192 34.5943967 161.247455 18.7734648"
-              ></polygon>
-              <polygon
-                id="cut3"
-                points="161.247455 18.7734648 237.005589 3.72423452e-12 328.899994 0 383.921967 30.8054072"
-              ></polygon>
-              <polygon
-                id="cut3"
-                points="383.921967 30.8054072 496.933492 40.8426061 504.822184 1.34410726e-12 476.802613 5.46933557e-05"
-              ></polygon>
-              <polygon
-                id="cut3"
-                points="524.864296 20.9690482 546.726829 51.565364 625.288283 30.8054072"
-              ></polygon>
-              <polygon
-                id="cut3"
-                points="625.288283 30.8054072 797.277649 5.46933556e-05 535.599911 8.15428954e-12"
-              ></polygon>
-              <polygon
-                id="cut3"
-                points="826.806552 40.8426061 895.225464 25.6117438 791.245314 5.46933572e-05"
-              ></polygon>
-              <polygon
-                id="cut3"
-                points="0 34.5943967 87.4561192 34.5943967 35.918129 0"
-              ></polygon>
-              <polygon
-                id="cut3"
-                points="161.247455 18.7734648 35.918129 -5.33573962e-13 173.938558 5.33573962e-13"
-              ></polygon>
-            </g>
-            <g
-              id="colortwo"
-              transform="translate(0.000000, 0.000000)"
-              fill="#3B1054"
-            >
-              <polygon
-                id="cut2"
-                points="166.540166 34.5943967 221.617527 78.0165342 150.44533 53.3410859"
-              ></polygon>
-              <polygon
-                id="cut2"
-                points="463.006014 30.8054072 358.998737 25.1855024 240.331502 18.7734648 354.181631 40.8426061"
-              ></polygon>
-              <polygon
-                id="cut2"
-                points="576.017538 40.8426061 603.948342 20.9690482 591.677754 1.0893284e-12 583.90623 0"
-              ></polygon>
-              <polygon
-                id="cut2"
-                points="704.372329 30.8054072 833.879562 66.0703089 905.890599 40.8426061"
-              ></polygon>
-              <polygon
-                id="cut2"
-                points="905.890599 40.8426061 928.174873 66.0703089 974.309511 25.6117438"
-              ></polygon>
-              <polygon
-                id="cut2"
-                points="0 1.34111174e-13 114.687191 -4.91615946e-13 79.0840464 34.5943967"
-              ></polygon>
-            </g>
-            <g
-              id="colorone"
-              transform="translate(79.084046, 0.000000)"
-              fill="#4A156A"
-            >
-              <polygon
-                id="cut1"
-                points="87.4561192 34.5943967 0 34.5943967 20.1458332 48.3949995"
-              ></polygon>
-              <polygon
-                id="cut1"
-                points="143.463804 78.0165342 275.097585 40.8426061 161.247455 18.7734648"
-              ></polygon>
-              <polygon
-                id="cut1"
-                points="275.097585 40.8426061 354.670819 66.0703089 383.921967 30.8054072"
-              ></polygon>
-              <polygon
-                id="cut1"
-                points="383.921967 30.8054072 329.509776 2.50729801e-12 476.802613 0"
-              ></polygon>
-              <polygon
-                id="cut1"
-                points="512.593708 1.36301164e-12 524.864296 20.9690482 625.288283 30.8054072 535.599911 0"
-              ></polygon>
-              <polygon
-                id="cut1"
-                points="625.288283 30.8054072 826.806552 40.8426061 791.245314 3.2908636e-13"
-              ></polygon>
-              <polygon
-                id="cut1"
-                points="791.245314 5.46933575e-05 939.946217 5.46933573e-05 895.225464 25.6117438"
-              ></polygon>
-              <polygon
-                id="cut1"
-                points="36.9257562 0 161.247455 18.7734648 87.4561192 34.5943967"
-              ></polygon>
-              <polygon
-                id="cut1"
-                points="173.938558 9.1828773e-13 161.247455 18.7734648 237.005589 1.17628168e-12"
-              ></polygon>
-            </g>
+            <polygon
+              id="cut3"
+              points="20.1458332 48.3949995 50.8965407 76.1330022 87.4561192 34.5943967"
+            ></polygon>
+            <polygon
+              id="cut3"
+              points="143.01306 78.0165342 87.4561192 34.5943967 161.247455 18.7734648"
+            ></polygon>
+            <polygon
+              id="cut3"
+              points="161.247455 18.7734648 237.005589 3.72423452e-12 328.899994 0 383.921967 30.8054072"
+            ></polygon>
+            <polygon
+              id="cut3"
+              points="383.921967 30.8054072 496.933492 40.8426061 504.822184 1.34410726e-12 476.802613 5.46933557e-05"
+            ></polygon>
+            <polygon
+              id="cut3"
+              points="524.864296 20.9690482 546.726829 51.565364 625.288283 30.8054072"
+            ></polygon>
+            <polygon
+              id="cut3"
+              points="625.288283 30.8054072 797.277649 5.46933556e-05 535.599911 8.15428954e-12"
+            ></polygon>
+            <polygon
+              id="cut3"
+              points="826.806552 40.8426061 895.225464 25.6117438 791.245314 5.46933572e-05"
+            ></polygon>
+            <polygon
+              id="cut3"
+              points="0 34.5943967 87.4561192 34.5943967 35.918129 0"
+            ></polygon>
+            <polygon
+              id="cut3"
+              points="161.247455 18.7734648 35.918129 -5.33573962e-13 173.938558 5.33573962e-13"
+            ></polygon>
           </g>
-          <polygon
-            id="piece4"
-            fill="#00B2E3"
-            transform="translate(133.668568, 15.831149) rotate(-12.000000) translate(-133.668568, -15.831149) "
-            points="112.657934 26.211127 154.679202 16.1739281 125.62247 5.45117017"
-          ></polygon>
-          <polygon
-            id="piece3"
-            fill="#00B2E3"
-            transform="translate(416.149603, 43.385088) rotate(10.000000) translate(-416.149603, -43.385088) "
-            points="391.545528 30.5282994 417.332449 56.2418774 440.753678 32.7397487"
-          ></polygon>
-          <polygon
-            id="piece2"
-            fill="#00B2E3"
-            transform="translate(544.029369, 30.467944) rotate(-9.000000) translate(-544.029369, -30.467944) "
-            points="520.410892 35.9448242 554.02425 38.211127 567.647845 22.724762"
-          ></polygon>
-          <polygon
-            id="piece1"
-            fill="#00B2E3"
-            transform="translate(866.120074, 19.847356) rotate(-4.000000) translate(-866.120074, -19.847356) "
-            points="868.286024 24.2080369 876.98582 15.4866753 855.254328 18.7000873"
-          ></polygon>
+          <g
+            id="colortwo"
+            transform="translate(566.084964, 39.008267) rotate(-180.000000) translate(-566.084964, -39.008267) translate(113.139665, 0.000000)"
+            fill="#3B1054"
+            stroke="#FFFFFF"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <polygon
+              id="cut2"
+              points="166.540166 34.5943967 221.617527 78.0165342 150.44533 53.3410859"
+            ></polygon>
+            <polygon
+              id="cut2"
+              points="463.006014 30.8054072 358.998737 25.1855024 240.331502 18.7734648 354.181631 40.8426061"
+            ></polygon>
+            <polygon
+              id="cut2"
+              points="576.017538 40.8426061 603.948342 20.9690482 591.677754 1.0893284e-12 583.90623 0"
+            ></polygon>
+            <polygon
+              id="cut2"
+              points="704.372329 30.8054072 833.879562 66.0703089 905.890599 40.8426061"
+            ></polygon>
+            <polygon
+              id="cut2"
+              points="0 1.34111174e-13 114.687191 -4.91615946e-13 79.0840464 34.5943967"
+            ></polygon>
+          </g>
+          <g
+            id="colorone"
+            transform="translate(469.973108, 39.008267) rotate(-180.000000) translate(-469.973108, -39.008267) "
+            fill="#4A156A"
+            stroke="#FFFFFF"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <polygon
+              id="cut1"
+              points="87.4561192 34.5943967 0 34.5943967 20.1458332 48.3949995"
+            ></polygon>
+            <polygon
+              id="cut1"
+              points="143.463804 78.0165342 275.097585 40.8426061 161.247455 18.7734648"
+            ></polygon>
+            <polygon
+              id="cut1"
+              points="275.097585 40.8426061 354.670819 66.0703089 383.921967 30.8054072"
+            ></polygon>
+            <polygon
+              id="cut1"
+              points="383.921967 30.8054072 329.509776 2.50729801e-12 476.802613 0"
+            ></polygon>
+            <polygon
+              id="cut1"
+              points="512.593708 1.36301164e-12 524.864296 20.9690482 625.288283 30.8054072 535.599911 0"
+            ></polygon>
+            <polygon
+              id="cut1"
+              points="625.288283 30.8054072 826.806552 40.8426061 791.245314 3.2908636e-13"
+            ></polygon>
+            <polygon
+              id="cut1"
+              points="791.245314 5.46933575e-05 939.946217 5.46933573e-05 895.225464 25.6117438"
+            ></polygon>
+            <polygon
+              id="cut1"
+              points="36.9257562 0 161.247455 18.7734648 87.4561192 34.5943967"
+            ></polygon>
+            <polygon
+              id="cut1"
+              points="173.938558 9.1828773e-13 161.247455 18.7734648 237.005589 1.17628168e-12"
+            ></polygon>
+          </g>
+          <g
+            id="dynamic"
+            transform="translate(462.270255, 34.772753) rotate(-180.000000) translate(-462.270255, -34.772753) translate(47.791297, 13.979400)"
+            fill="#2C60F0"
+          >
+            <polygon
+              id="piece1"
+              points="8.16427974 31.8906577 4.54747351e-13 41.5867055 34.3809073 41.5867055"
+            ></polygon>
+            <polygon
+              id="piece4"
+              points="764.984711 19.2797979 790.252853 35.8822301 828.957916 4.85897573"
+            ></polygon>
+            <polygon
+              id="piece3"
+              points="436.016857 17.7555084 461.667292 0 480.987996 28.3733292"
+            ></polygon>
+            <polygon
+              id="piece2"
+              points="211.900581 20.0688833 241.306458 38.2602971 249.30072 32.1793111"
+            ></polygon>
+          </g>
         </g>
       </g>
     </g>
