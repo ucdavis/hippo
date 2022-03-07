e5550d5024e0179b2a5d9ead305738feb368e05a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-03-07 09:22:04 -0800

    ShowFor

---
+++ Hippo.Web/ClientApp/src/Shared/ShowFor.tsx
@@ -0,0 +1,47 @@
+import React, { useContext, useMemo } from "react";
+
+import AppContext from "./AppContext";
+import { RoleName } from "../types";
+import { isBoolean, isFunction } from "../util/TypeChecks";
+
+interface Props {
+  children: any;
+  roles: RoleName[];
+  condition?: boolean | (() => boolean);
+}
+
+export const ShowFor = (props: Props) => {
+  const { children, roles } = props;
+  const [context] = useContext(AppContext);
+  const systemUsers = ["jsylvest", "postit", "cydoval", "sweber"];
+  const conditionSatisfied = isBoolean(props.condition)
+    ? props.condition
+    : isFunction(props.condition)
+    ? props.condition()
+    : true;
+
+  if (
+    conditionSatisfied &&
+    systemUsers.includes(context.user.detail.kerberos)
+  ) {
+    return <>{children}</>;
+  }
+
+  if (conditionSatisfied && roles.includes("Sponsor")) {
+    if (context.account.canSponsor) {
+      return <>{children}</>;
+    }
+  }
+
+  if (conditionSatisfied && roles.includes("Admin")) {
+    if (context.user.detail.isAdmin) {
+      return <>{children}</>;
+    }
+  }
+
+  return null;
+};
+
+// Can be used as either a hook or a component. Exporting under
+// seperate name as a reminder that rules of hooks still apply.
+export const useFor = ShowFor;
