d5fe82ec5c2ec3c4fff83bf0f63fdfc83449cc60
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-03-17 10:57:26 -0700

    Green to purple
    mjml files first

--- Hippo.Email/Models/DecisionModel.cs
+++ Hippo.Email/Models/DecisionModel.cs
@@ -13,18 +13,18 @@ namespace Hippo.Email.Models
         public string RequesterName { get; set; } = String.Empty;
         public string ClusterName { get; set; } = "FARM/CAES";
         public string RequestDate { get; set; } = String.Empty;
-        public string DecisionDate { get; set; } = String.Empty; 
+        public string DecisionDate { get; set; } = String.Empty;
         public string Instructions { get; set; } = "Your account request has been approved. Once it has been processed you will have access. You can check View Account to see the current status of your account.";
         public string RequestUrl { get; set; } = String.Empty;
         public string ButtonText { get; set; } = "View Account";
 
-        public string Decision { get; set; } = String.Empty ;
+        public string Decision { get; set; } = String.Empty;
         public string DecisionColor { get; set; } = Colors.Approved;
         public string Reason { get; set; } = String.Empty;
 
         public class Colors
         {
-            public const string Approved = "#266041";
+            public const string Approved = "#266041"; //Change to 481268 (Purple for approved?)
             public const string Rejected = "#cf3c3c";
 
         }
