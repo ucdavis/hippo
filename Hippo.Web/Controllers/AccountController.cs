9fa9bbc6e6484d7c496db92022657a545eff3473
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-03-18 11:04:25 -0700

    Just check it starts with ssh-

--- Hippo.Web/Controllers/AccountController.cs
+++ Hippo.Web/Controllers/AccountController.cs
@@ -158,7 +158,7 @@ public class AccountController : SuperController
         {
             return BadRequest("Missing SSH Key");
         }
-        if (!model.SshKey.StartsWith("ssh-rsa ") || model.SshKey.Trim().Length < 25)
+        if (!model.SshKey.StartsWith("ssh-") || model.SshKey.Trim().Length < 25)
         {
             return BadRequest("Invalid SSH key");
         }
