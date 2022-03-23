31a45e978847b1036b91edcc98411ccafaeb0bfd
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-03-23 12:51:08 -0700

    Add cluster to history DB

---
+++ Hippo.Core/Migrations/Sqlite/20220323194054_clusterHistory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hippo.Core.Migrations.Sqlite
+{
+    public partial class clusterHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClusterId",
+                table: "Histories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Histories_ClusterId",
+                table: "Histories",
+                column: "ClusterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Histories_Clusters_ClusterId",
+                table: "Histories",
+                column: "ClusterId",
+                principalTable: "Clusters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Histories_Clusters_ClusterId",
+                table: "Histories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Histories_ClusterId",
+                table: "Histories");
+
+            migrationBuilder.DropColumn(
+                name: "ClusterId",
+                table: "Histories");
+        }
+    }
+}
