d39984600b8fb0deea590a440366ec6882f13cf9
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2023-07-10 12:47:02 -0700

    Add PuppetService for retrieving users and groups

---
+++ Hippo.Core/Models/Settings/PuppetSettings.cs
@@ -0,0 +1,11 @@
+
+namespace Hippo.Core.Models.Settings
+{
+    public class PuppetSettings
+    {
+        public string RepositoryOwner { get; set; }
+        public string RepositoryName { get; set; }
+        public string RepositoryBranch { get; set; }
+        public string AuthToken { get; set; }
+    }
+}
