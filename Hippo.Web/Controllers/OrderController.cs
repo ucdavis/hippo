8363f0f90cf84bc35bfdee3dcd011a66f19dc80c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-05-03 13:16:02 -0700

    WIP validation

---
+++ Hippo.Web/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using Hippo.Core.Models;
+using Hippo.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hippo.Web.Controllers
+{
+
+    [Authorize]
+    public class OrderController : SuperController
+    {
+        private IAggieEnterpriseService _aggieEnterpriseService;
+
+        public OrderController(IAggieEnterpriseService aggieEnterpriseService)
+        {
+            _aggieEnterpriseService = aggieEnterpriseService;
+        }
+
+        [HttpGet]
+        [Route("api/order/validate-chart-string/{chartString}")]
+        public async Task<ChartStringValidationModel> ValidateChartString(string chartString)
+        {
+            return await _aggieEnterpriseService.IsChartStringValid(chartString);
+        }
+
+    }
+}
