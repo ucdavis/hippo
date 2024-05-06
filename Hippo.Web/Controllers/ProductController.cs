808c3b516d417c2f156d1ff8936d69335b1daa21
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-05-06 13:28:33 -0700

    WIP Products

---
+++ Hippo.Web/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+﻿using Hippo.Core.Data;
+using Hippo.Core.Domain;
+using Hippo.Core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hippo.Web.Controllers
+{
+    [Authorize]
+    public class ProductController : SuperController
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ProductController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _dbContext.Products.Where(a => a.Cluster.Name == Cluster).ToListAsync(); //Filters out inactive products
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Policy = AccessCodes.ClusterAdminAccess)]
+        public async Task<IActionResult> CreateProduct(Product model)
+        {
+            var cluster = await _dbContext.Clusters.FirstAsync(a => a.Name == Cluster);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid");
+            }
+
+            var procuct = new Product
+            {
+                Name = model.Name,
+                Description = model.Description,
+                Category = model.Category,
+                UnitPrice = model.UnitPrice,
+                Units = model.Units,
+                Cluster = cluster,
+                Installments = model.Installments
+            };
+            _dbContext.Products.Add(procuct);
+            await _dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(a => a.Id == id && a.Cluster.Name == Cluster);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = AccessCodes.ClusterAdminAccess)]
+        public async Task<IActionResult> UpdateProduct(Product model)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(a => a.Id == model.Id && a.Cluster.Name == Cluster);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            product.Name = model.Name;
+            product.Description = model.Description;
+            product.Category = model.Category;
+            product.UnitPrice = model.UnitPrice;
+            product.Units = model.Units;
+            product.Installments = model.Installments;
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Authorize(Policy = AccessCodes.ClusterAdminAccess)]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(a => a.Id == id && a.Cluster.Name == Cluster);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            product.IsActive = false;
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
