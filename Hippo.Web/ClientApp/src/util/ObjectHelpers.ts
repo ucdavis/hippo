fafa2f77dadbd1c797e799859b47bb0060f76346
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2024-05-21 16:07:21 -0700

    Don't try to find properties on null/undefined objects

--- Hippo.Web/ClientApp/src/util/ObjectHelpers.ts
+++ Hippo.Web/ClientApp/src/util/ObjectHelpers.ts
@@ -1,6 +1,8 @@
 export const filterCommonProperties = <T extends object>(obj: T) => {
   return filterObjectTree(obj, (key, value) => {
     if (key === "id" || key === "uid" || key === "gid") return false;
+    if ((Array.isArray(value) || typeof value === "object") && !value)
+      return false; // don't display property for empty/null object
     return true;
   });
 };
@@ -15,7 +17,7 @@ export const filterObjectTree = <T, K extends keyof T>(
     .reduce((o, k) => {
       const rawValue = obj[k];
       const value =
-        Array.isArray(rawValue) || typeof rawValue === "object"
+        (Array.isArray(rawValue) || typeof rawValue === "object") && !!rawValue
           ? filterObjectTree(rawValue, filter)
           : rawValue;
       o[k] = value;
