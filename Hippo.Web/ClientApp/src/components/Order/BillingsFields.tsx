a8656949643eb5325209e67eb8e7ead18a749f6c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-06-20 08:06:10 -0700

    Use a table instead

--- Hippo.Web/ClientApp/src/components/Order/BillingsFields.tsx
+++ Hippo.Web/ClientApp/src/components/Order/BillingsFields.tsx
@@ -3,10 +3,9 @@ import { useFieldArray, useFormContext } from "react-hook-form";
 import { OrderModel } from "../../types";
 
 import FormField from "../../Shared/Form/FormField";
-import { Row, Col } from "reactstrap";
 import HipButton from "../../Shared/HipButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faTrash, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 type BillingsFieldsProps = {
   readOnly: boolean;
@@ -51,52 +50,73 @@ const BillingsFields: React.FC<BillingsFieldsProps> = ({ readOnly }) => {
   return (
     <>
       <h2>Billing Info</h2>
-      {fields.map((field, index) => {
-        return (
-          <Row key={field.id}>
-            <Col>
-              <div className="input-group">
-                <FormField
-                  key={`chartString-${field.id}`}
-                  register={register}
-                  label="ChartString"
-                  error={errors.billings?.[index]?.chartString}
-                  name={`billings.${index}.chartString`}
-                  autoComplete="nope"
-                  readOnly={readOnly}
-                />
-                <button className="btn btn-primary" type="button">
-                  <i className="fas fa-search"></i>
-                </button>
-              </div>
-            </Col>
-            <Col>
-              <FormField
-                key={`percentage-${field.id}`}
-                register={register}
-                label="Percentage"
-                error={errors.billings?.[index]?.percentage}
-                name={`billings.${index}.percentage`}
-                readOnly={readOnly}
-              />
-            </Col>
-            {!readOnly && (
-              <Col md={3}>
-                <HipButton
-                  title="Remove ChartString"
-                  aria-label="Remove ChartString"
-                  color="danger"
-                  outline={true}
-                  size="sm"
-                  onClick={() => removeBilling(index)}
-                >
-                  <FontAwesomeIcon icon={faTrash} />
-                </HipButton>
-              </Col>
-            )}
-          </Row>
-        );
-      })}
+      <h2>Chart Strings</h2>
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>Chart String</th>
+            <th>Percent</th>
+            <th>Chart String Validation</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {fields.map((field, index) => {
+            return (
+              <tr key={field.id}>
+                <td>
+                  <FormField
+                    key={`chartString-${field.id}`}
+                    register={register}
+                    label=""
+                    error={errors.billings?.[index]?.chartString}
+                    name={`billings.${index}.chartString`}
+                    autoComplete="nope"
+                    readOnly={readOnly}
+                  />
+                </td>
+                <td width={"40px;"}>
+                  <FormField
+                    key={`percentage-${field.id}`}
+                    register={register}
+                    label=""
+                    error={errors.billings?.[index]?.percentage}
+                    name={`billings.${index}.percentage`}
+                    readOnly={readOnly}
+                  />
+                </td>
+                <td></td>
+                {!readOnly && (
+                  <td width={"160px;"}>
+                    <HipButton
+                      title="Lookup ChartString"
+                      aria-label="Lookup ChartString"
+                      color="primary"
+                      outline={true}
+                      size="sm"
+                      onClick={() => alert(index)}
+                    >
+                      <FontAwesomeIcon icon={faSearch} />
+                    </HipButton>{" "}
+                    <HipButton
+                      title="Remove ChartString"
+                      aria-label="Remove ChartString"
+                      color="danger"
+                      outline={true}
+                      size="sm"
+                      onClick={() => removeBilling(index)}
+                    >
+                      <FontAwesomeIcon icon={faTrash} />
+                    </HipButton>
+                  </td>
+                )}
+                {readOnly && <td width={"40px;"}></td>}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
       {!readOnly && (
         <HipButton outline={true} color="secondary" onClick={addBilling}>
           <FontAwesomeIcon icon={faPlus} size="sm" /> Add Billing
