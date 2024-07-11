4ae132cc7b191695a9af411afa29d59c4a00d1e2
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-07-11 14:51:33 -0700

    Migration to get the Order.OnModelCreating Working

--- Hippo.Core/Domain/History.cs
+++ Hippo.Core/Domain/History.cs
@@ -50,7 +50,7 @@ namespace Hippo.Core.Domain
             modelBuilder.Entity<History>().HasIndex(h => h.Type);
             modelBuilder.Entity<History>().HasOne(h => h.ActedBy).WithMany().HasForeignKey(a => a.ActedById).OnDelete(DeleteBehavior.Restrict);
             modelBuilder.Entity<History>().HasOne(h => h.Cluster).WithMany().HasForeignKey(a => a.ClusterId).OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<History>().HasOne(h => h.Order).WithMany().HasForeignKey(a => a.OrderId).OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<History>().HasOne(h => h.Order).WithMany(o => o.History).HasForeignKey(a => a.OrderId).OnDelete(DeleteBehavior.Restrict);
         }
 
         public class Actions
---
+++ Hippo.Core/Migrations/SqlServer/20240711213919_OrderHistory.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hippo.Core.Migrations.SqlServer
+{
+    public partial class OrderHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Billings_Orders_OrderId",
+                table: "Billings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Histories_Orders_OrderId",
+                table: "Histories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MetaData_Orders_OrderId",
+                table: "MetaData");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Accounts_PrincipalInvestigatorId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Orders_OrderId",
+                table: "Payments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Billings_Orders_OrderId",
+                table: "Billings",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Histories_Orders_OrderId",
+                table: "Histories",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MetaData_Orders_OrderId",
+                table: "MetaData",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Accounts_PrincipalInvestigatorId",
+                table: "Orders",
+                column: "PrincipalInvestigatorId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Orders_OrderId",
+                table: "Payments",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Billings_Orders_OrderId",
+                table: "Billings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Histories_Orders_OrderId",
+                table: "Histories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MetaData_Orders_OrderId",
+                table: "MetaData");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Accounts_PrincipalInvestigatorId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Orders_OrderId",
+                table: "Payments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Billings_Orders_OrderId",
+                table: "Billings",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Histories_Orders_OrderId",
+                table: "Histories",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MetaData_Orders_OrderId",
+                table: "MetaData",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Accounts_PrincipalInvestigatorId",
+                table: "Orders",
+                column: "PrincipalInvestigatorId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Orders_OrderId",
+                table: "Payments",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
