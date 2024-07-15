1761bf73e79c5990ba98669feb7ccb10c789071a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-07-15 08:25:12 -0700

    Sloth Service

---
+++ Hippo.Core/Services/SlothService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hippo.Core.Services
+{
+    public interface ISlothService
+    {
+        //Move money
+        //Check Txns
+        //Test API key
+    }
+    public class SlothService : ISlothService
+    {
+    }
+}
