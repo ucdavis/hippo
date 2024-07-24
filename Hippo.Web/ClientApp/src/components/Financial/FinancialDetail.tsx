d8df60afde89dcb23a24dd7cb635bccc05ab7ea8
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-07-24 09:44:32 -0700

    cleanup

--- Hippo.Web/ClientApp/src/components/Financial/FinancialDetail.tsx
+++ Hippo.Web/ClientApp/src/components/Financial/FinancialDetail.tsx
@@ -68,14 +68,10 @@ export const FinancialDetail = () => {
       },
     );
     if ((await request).ok) {
-      // refresh the page
-
+      // refresh the data
       const data = await (await request).json();
-      console.log(data);
       setFinancialDetail(data);
       await validateChartString(data.chartString);
-      //debugger;
-      //window.location.reload();
     }
   };
 
@@ -103,10 +99,9 @@ export const FinancialDetail = () => {
         method: "GET",
       },
     );
-    //console.log(response);
+
     if (response.ok) {
       const result = await response.json();
-      console.log(result);
       setChartStringValidation(result);
       if (result.chartString) {
         setFinancialDetail((prevFinancialDetail) => ({
