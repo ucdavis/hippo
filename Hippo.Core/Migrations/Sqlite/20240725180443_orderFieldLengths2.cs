570b60a56c22cb3c7335ece64017ebbb483f5bfc
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-07-25 11:05:31 -0700

    Migration

---
+++ Hippo.Core/Migrations/Sqlite/20240725180443_orderFieldLengths2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hippo.Core.Migrations.Sqlite
+{
+    public partial class orderFieldLengths2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
