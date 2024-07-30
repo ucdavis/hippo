b630555c7004857e0e2cc63d66c1b1e8132e68d4
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2024-07-30 16:28:43 -0700

    A bit of cleanup

--- Hippo.Web/Controllers/TagsController.cs
+++ Hippo.Web/Controllers/TagsController.cs
@@ -47,35 +47,24 @@ public class TagsController : SuperController
     public async Task<ActionResult> UpdateAccountTags([FromBody] AccountTagsModel accountTagsModel)
     {
         if (string.IsNullOrWhiteSpace(Cluster))
-        {
             return BadRequest("Cluster is required");
-        }
-
         if (accountTagsModel == null)
-        {
             return BadRequest("AccountTagsModel is required");
-        }
-
         if (!ModelState.IsValid)
-        {
             return BadRequest(ModelState);
-        }
 
         var account = await _dbContext.Accounts
             .Include(a => a.Tags)
             .FirstOrDefaultAsync(a => a.Id == accountTagsModel.AccountId);
         if (account == null)
-        {
             return NotFound("Account not found");
-        }
 
         var permissions = await _userService.GetCurrentPermissionsAsync();
         var canEditTags = permissions.IsClusterOrSystemAdmin(Cluster);
-
-        var currentUser = await _userService.GetCurrentUser();
         if (!canEditTags)
         {
             // check if user is a PI/Sponsor for given account
+            var currentUser = await _userService.GetCurrentUser();
             canEditTags = await _dbContext.Accounts.AnyAsync(
             a => a.Cluster.Name == Cluster
             && a.OwnerId == currentUser.Id
@@ -85,9 +74,7 @@ public class TagsController : SuperController
         }
 
         if (!canEditTags)
-        {
             return Unauthorized();
-        }
 
         var existingTags = await _dbContext.Tags
             .Where(t => t.Cluster.Name == Cluster && accountTagsModel.Tags.Contains(t.Name))
@@ -101,12 +88,11 @@ public class TagsController : SuperController
             })
             .ToArray();
 
-        // set desired state of account tags and le change tracker determine what to do.
+        // set desired state of account tags and let change tracker determine what to do.
         account.Tags.Clear();
         account.Tags.AddRange(existingTags);
         account.Tags.AddRange(createTags);
         await _dbContext.SaveChangesAsync();
         return Ok();
     }
-
 }
