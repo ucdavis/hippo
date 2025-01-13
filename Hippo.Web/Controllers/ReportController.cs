664ede8b69222fefe53f14c806aa15c6ca6d4e20
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-01-13 08:02:12 -0800

    Server side filters

--- Hippo.Web/Controllers/ReportController.cs
+++ Hippo.Web/Controllers/ReportController.cs
@@ -46,25 +46,29 @@ namespace Hippo.Web.Controllers
             }
 
             var query = _dbContext.Payments.Include(a => a.Order.PrincipalInvestigator).Include(a => a.Order.MetaData).Where(a => a.Order.Cluster.Name == Cluster && a.Status == Payment.Statuses.Completed && a.CompletedOn != null);
-            if(start != null && start != "undefined")
+            var orderQuery = _dbContext.Orders.Where(a => a.Cluster.Name == Cluster); //Will only be used for the filter using order info
+            List<int> orderIds = new List<int>();
+
+            DateTime? startDate = null;
+            DateTime? endDate = null;
+
+            if (!string.IsNullOrWhiteSpace(start) && start != "undefined")
             {
                 try
                 {
-                    var date = DateTime.Parse(start).ToUniversalTime();
-                    query = query.Where(a => a.CompletedOn >= date);
+                    startDate = DateTime.Parse(start).ToUniversalTime();
                 }
                 catch (Exception e)
                 {
                     Log.Error(e, "Error parsing start date");
                 }
-                
+
             }
-            if (end != null && end != "undefined")
+            if (!string.IsNullOrWhiteSpace(end) && end != "undefined")
             {
                 try
                 {
-                    var date = DateTime.Parse(end).ToUniversalTime();
-                    query = query.Where(a => a.CompletedOn <= date);
+                    endDate = DateTime.Parse(end).ToUniversalTime();
                 }
                 catch (Exception e)
                 {
@@ -72,6 +76,61 @@ namespace Hippo.Web.Controllers
                 }
 
             }
+
+            switch (filterType)
+            {
+                case "PaymentDate":
+                    if (startDate.HasValue)
+                    {
+                        query = query.Where(a => a.CompletedOn >= startDate.Value);
+                    }
+                    if (endDate.HasValue)
+                    {
+                        query = query.Where(a => a.CompletedOn <= endDate.Value);
+                    }
+                    break;
+                case "OrderExpiryDate":
+                    if (startDate.HasValue)
+                    {
+                        orderQuery = orderQuery.Where(a => a.ExpirationDate != null && a.ExpirationDate >= startDate.Value);
+                    }
+                    if (endDate.HasValue)
+                    {
+                        orderQuery = orderQuery.Where(a => a.ExpirationDate != null && a.ExpirationDate <= endDate.Value);
+                    }
+                    orderIds = await orderQuery.Select(a => a.Id).ToListAsync();
+                    query = query.Where(a => orderIds.Contains(a.OrderId));
+                    break;
+                case "OrderInstallmentDate":
+                    if (startDate.HasValue)
+                    {
+                        orderQuery = orderQuery.Where(a => a.InstallmentDate != null && a.InstallmentDate >= startDate.Value);
+                    }
+                    if (endDate.HasValue)
+                    {
+                        orderQuery = orderQuery.Where(a => a.InstallmentDate != null && a.InstallmentDate <= endDate.Value);
+                    }
+                    orderIds = await orderQuery.Select(a => a.Id).ToListAsync();
+                    query = query.Where(a => orderIds.Contains(a.OrderId));
+                    break;
+                case "OrderCreationDate":
+                    if (startDate.HasValue)
+                    {
+                        orderQuery = orderQuery.Where(a => a.CreatedOn >= startDate.Value);
+                    }
+                    if (endDate.HasValue)
+                    {
+                        orderQuery = orderQuery.Where(a => a.CreatedOn <= endDate.Value);
+                    }
+                    orderIds = await orderQuery.Select(a => a.Id).ToListAsync();
+                    query = query.Where(a => orderIds.Contains(a.OrderId));
+                    break;
+                default:
+                    return BadRequest("Invalid filter type.");
+            }
+
+
+
 
             //var payments = await _dbContext.Payments.Include(a => a.Order.PrincipalInvestigator).Include(a => a.Order.MetaData).Where(a => a.Order.Cluster.Name == Cluster && a.Status == Payment.Statuses.Completed && ids.Contains(a.OrderId)).Select(InvoiceModel.Projection()).ToListAsync();
             //var payments2 = await _dbContext.Payments.Include(a => a.Order.PrincipalInvestigator).Include(a => a.Order.MetaData).Where(a => a.Status == Payment.Statuses.Completed).Select(InvoiceModel.Projection()).ToListAsync();
