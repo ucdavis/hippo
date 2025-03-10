a3ac3b0257feae3938e001945e0ade242a9699ed
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-03-10 10:26:45 -0700

    refactor for readability

--- Hippo.Web/Controllers/ReportController.cs
+++ Hippo.Web/Controllers/ReportController.cs
@@ -178,21 +178,55 @@ namespace Hippo.Web.Controllers
             {
                 return BadRequest("You do not have permission to view this page.");
             }
-            var orders = await _dbContext.Orders.Include(a => a.Billings).Where(a => a.Cluster.Name == Cluster && a.Status == Order.Statuses.Active).ToListAsync();
 
-            var uniqueChartStrings = orders.SelectMany(a => a.Billings.Where(a => a.ChartString != null).Select(b => b.ChartString)).Distinct().ToList();
+            var orders = await _dbContext.Orders
+                .Include(a => a.Billings)
+                .Where(a => a.Cluster.Name == Cluster && a.Status == Order.Statuses.Active)
+                .ToListAsync();
 
-            //Check for and validate these order's chart strings
+            var invalidChartStringOrWarnings = await ValidateChartStrings(orders);
+
+            var problemOrders = orders
+                .Where(a => a.Billings.Any(b => invalidChartStringOrWarnings.Any(c => c.ContainsKey(b.ChartString))))
+                .ToList();
+
+            var billingIssues = orders
+                .Where(a => a.NextPaymentDate <= DateTime.UtcNow.AddDays(5))
+                .ToList();
+
+            var orderIds = problemOrders.Select(a => a.Id).Union(billingIssues.Select(a => a.Id)).ToList();
+
+            var model = await _dbContext.Orders
+                .Include(a => a.PrincipalInvestigator)
+                .Include(a => a.MetaData)
+                .Where(a => orderIds.Contains(a.Id))
+                .Select(OrderListModel.Projection())
+                .ToListAsync();
+
+            AddMessagesToModel(problemOrders, invalidChartStringOrWarnings, model);
+            AddBillingIssueMessages(billingIssues, model);
+
+            return Ok(model);
+        }
+
+        private async Task<List<Dictionary<string, string>>> ValidateChartStrings(List<Order> orders)
+        {
+            var uniqueChartStrings = orders
+                .SelectMany(a => a.Billings.Where(a => a.ChartString != null).Select(b => b.ChartString))
+                .Distinct()
+                .ToList();
+
             var invalidChartStringOrWarnings = new List<Dictionary<string, string>>();
 
             foreach (var chartString in uniqueChartStrings)
             {
-
                 if (chartString == null)
                 {
                     continue;
                 }
+
                 var chartStringValidation = await _aggieEnterpriseService.IsChartStringValid(chartString, Directions.Debit);
+
                 if (!chartStringValidation.IsValid)
                 {
                     var invalidEntry = new Dictionary<string, string> { { chartString, chartStringValidation.Message } };
@@ -204,51 +238,37 @@ namespace Hippo.Web.Controllers
                     {
                         var invalidEntry = new Dictionary<string, string> { { chartString, chartStringValidation.Warning } };
                         invalidChartStringOrWarnings.Add(invalidEntry);
+
                     }
                 }
-
             }
 
-
-            //Find all orders that have a billing with a chart string that is in the invalidChartStringOrWarnings
-            var problemOrders = orders.Where(a => a.Billings.Any(b => invalidChartStringOrWarnings.Any(c => c.ContainsKey(b.ChartString)))).ToList();
-
-
-            var billingIssues = orders.AsQueryable().Where(a => a.NextPaymentDate <= DateTime.UtcNow.AddDays(5)).ToList();
-
-            //get a list of order ids and combile them from both problemOrders and billingIssues
-            var orderIds = problemOrders.Select(a => a.Id).Union(billingIssues.Select(a => a.Id)).ToList();
-
-            var model = await _dbContext.Orders.Include(a => a.PrincipalInvestigator).Include(a => a.MetaData).Where(a => orderIds.Contains(a.Id)).Select(OrderListModel.Projection()).ToListAsync();
-
-            if(problemOrders.Count > 0)
-            {
-                foreach (var order in problemOrders)
-                {
-                    var messages = new List<string>();
-                    messages.Add("Chart String Problem:");
-                    foreach (var billing in order.Billings)
-                    {
-                        //check if the chart string is in the invalidChartStringOrWarnings
-                        if (invalidChartStringOrWarnings.Any(a => a.ContainsKey(billing.ChartString)))
-                        {
-                            messages.Add(invalidChartStringOrWarnings.First(a => a.ContainsKey(billing.ChartString))[billing.ChartString]);
-                        }
-                    }
-                    //get the order from the model and add the messages
-                    model.First(a => a.Id == order.Id).Messages = string.Join(" ", messages);
-                }
-            }
-            if(billingIssues.Count > 0)
-            {
-                foreach (var order in billingIssues)
-                {
-                    //get the order from the model and add the messages
-                    model.First(a => a.Id == order.Id).Messages = $"Stale Next Payment Date. {model.First(a => a.Id == order.Id).Messages}";
-                }
-            }
-
-            return Ok(model);
+            return invalidChartStringOrWarnings;
         }
+
+        private void AddMessagesToModel(List<Order> problemOrders, List<Dictionary<string, string>> invalidChartStringOrWarnings, List<OrderListModel> model)
+        {
+            foreach (var order in problemOrders)
+            {
+                var messages = new List<string> { "Chart String Problem:" };
+                foreach (var billing in order.Billings)
+                {
+                    if (invalidChartStringOrWarnings.Any(a => a.ContainsKey(billing.ChartString)))
+                    {
+                        messages.Add(invalidChartStringOrWarnings.First(a => a.ContainsKey(billing.ChartString))[billing.ChartString]);
+                    }
+                }
+                model.First(a => a.Id == order.Id).Messages = string.Join(" ", messages);
+            }
+        }
+
+        private void AddBillingIssueMessages(List<Order> billingIssues, List<OrderListModel> model)
+        {
+            foreach (var order in billingIssues)
+            {
+                model.First(a => a.Id == order.Id).Messages = $"Stale Next Payment Date. {model.First(a => a.Id == order.Id).Messages}";
+            }
+        }
+
     }
 }
