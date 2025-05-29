d07a8c09c3e3c9a36a7e2ae436ecbaabfe334cf3
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-05-29 12:12:58 -0700

    New DB field to track if the rate was changed

---
+++ Hippo.Core/Migrations/Sqlite/20250529182428_ChangeRate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hippo.Core.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class ChangeRate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "WasRateAdjusted",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WasRateAdjusted",
+                table: "Orders");
+        }
+    }
+}
